da5930460320d3321d36c3434ba41108a85d9c58
Daniil Rezchikov <rezdan29@gmail.com>
2025-11-18 10:01:59 +0300

    Implement loss masking and saving weights of trained models

--- train_with_loss_mask.py
+++ train_with_loss_mask.py
@@ -1,4 +1,4 @@
-from models.autoencoder import Autoencoder, PathLogger
+from models.autoencoder_with_loss_mask import Autoencoder, PathLogger
 from data.hmaps import GridData
 
 import pytorch_lightning as pl
@@ -12,6 +12,16 @@ import argparse
 import multiprocessing
 
 
+def build_loss_mask(maps, starts, goals, mode):
+    mask = torch.ones_like(maps)
+
+    mask *= (maps == 0)
+    mask *= (goals == 0)
+    if mode == "f":
+        mask *= (starts == 0)
+
+    return mask.float()
+
 def main(mode, run_name, proj_name, batch_size, max_epochs):
     train_data = GridData(
         path='./TransPath_data/train',
@@ -22,24 +32,24 @@ def main(mode, run_name, proj_name, batch_size, max_epochs):
         mode=mode
     )
     resolution = (train_data.img_size, train_data.img_size)
-    train_dataloader = DataLoader(  train_data,
-                                    batch_size=batch_size,
-                                    shuffle=True,
-                                    num_workers=multiprocessing.cpu_count(),
-                                    pin_memory=True)
-    val_dataloader = DataLoader(    val_data,
-                                    batch_size=batch_size,
-                                    shuffle=False,
-                                    num_workers=multiprocessing.cpu_count(),
-                                    pin_memory=True)
+    train_dataloader = DataLoader(train_data,
+                                  batch_size=batch_size,
+                                  shuffle=True,
+                                  num_workers=multiprocessing.cpu_count(),
+                                  pin_memory=True)
+    val_dataloader = DataLoader(val_data,
+                                batch_size=batch_size,
+                                shuffle=False,
+                                num_workers=multiprocessing.cpu_count(),
+                                pin_memory=True)
 
     samples = next(iter(val_dataloader))
-    
+
     model = Autoencoder(mode=mode, resolution=resolution)
     callback = PathLogger(samples, mode=mode)
     wandb_logger = WandbLogger(project=proj_name, name=f'{run_name}_{mode}', log_model='all')
     checkpoint_callback = ModelCheckpoint(
-        dirpath="checkpoints/without_loss_mask",
+        dirpath="checkpoints/with_loss_mask",
         filename="best",
         save_top_k=1,
         monitor="val_loss",
@@ -59,13 +69,14 @@ def main(mode, run_name, proj_name, batch_size, max_epochs):
         ckpt = torch.load(best_ckpt, map_location="cuda")
         state_dict = ckpt["state_dict"]
         if mode == "f":
-            torch.save(state_dict, "weights/best_weights_without_loss_mask_f.pth")
-            print("Saved best model to best_weights_without_loss_mask_f.pth")
+            torch.save(state_dict, "weights/best_weights_with_loss_mask_f.pth")
+            print("Saved best model to best_weights_with_loss_mask_f.pth")
         else:
-            torch.save(state_dict, "weights/best_weights_without_loss_mask_cf.pth")
-            print("Saved best model to best_weights_without_loss_mask_cf.pth")
+            torch.save(state_dict, "weights/best_weights_with_loss_mask_cf.pth")
+            print("Saved best model to best_weights_with_loss_mask_cf.pth")
     wandb.finish()
-    
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', type=str, choices=['f', 'cf'], default='cf')
@@ -74,10 +85,10 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=39)
     parser.add_argument('--batch', type=int, default=256)
     parser.add_argument('--epoch', type=int, default=160)
-    
+
     args = parser.parse_args()
     pl.seed_everything(args.seed)
-    torch.set_float32_matmul_precision('high') #fix for tensor blocks warning with new video card
+    torch.set_float32_matmul_precision('high')  # fix for tensor blocks warning with new video card
     main(
         mode=args.mode,
         run_name=args.run_name,
