28f9f22c6232779331e52a486244f33b58374b9f
Daniil Rezchikov <rezdan29@gmail.com>
2025-11-18 06:58:33 +0300

    Clear repository from DEM

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
-from models.autoencoder import Autoencoder, PathLogger, DemAutoencoder, DemPathLogger
+from models.autoencoder import Autoencoder, PathLogger
 from data.hmaps import GridData
-from data.dems import DemData
 
 import pytorch_lightning as pl
 import wandb
@@ -16,27 +15,27 @@ def main(mode, run_name, proj_name, batch_size, max_epochs):
     train_data = GridData(
         path='./TransPath_data/train',
         mode=mode
-    ) if mode != 'dem' else DemData(split='train')
+    )
     val_data = GridData(
         path='./TransPath_data/val',
         mode=mode
-    ) if mode != 'dem' else DemData(split='val')
+    )
     resolution = (train_data.img_size, train_data.img_size)
-    train_dataloader = DataLoader(  train_data, 
+    train_dataloader = DataLoader(  train_data,
                                     batch_size=batch_size,
-                                    shuffle=True, 
-                                    num_workers=multiprocessing.cpu_count(), 
+                                    shuffle=True,
+                                    num_workers=multiprocessing.cpu_count(),
                                     pin_memory=True)
-    val_dataloader = DataLoader(    val_data, 
+    val_dataloader = DataLoader(    val_data,
                                     batch_size=batch_size,
-                                    shuffle=False, 
-                                    num_workers=multiprocessing.cpu_count(), 
+                                    shuffle=False,
+                                    num_workers=multiprocessing.cpu_count(),
                                     pin_memory=True)
-    
+
     samples = next(iter(val_dataloader))
     
-    model = Autoencoder(mode=mode, resolution=resolution) if mode != 'dem' else DemAutoencoder(resolution=resolution)
-    callback = PathLogger(samples, mode=mode) if mode != 'dem' else DemPathLogger(samples)
+    model = Autoencoder(mode=mode, resolution=resolution)
+    callback = PathLogger(samples, mode=mode)
     wandb_logger = WandbLogger(project=proj_name, name=f'{run_name}_{mode}', log_model='all')
     trainer = pl.Trainer(
         logger=wandb_logger,
@@ -50,7 +49,7 @@ def main(mode, run_name, proj_name, batch_size, max_epochs):
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', type=str, choices=['f', 'cf', 'dem'], default='dem')
+    parser.add_argument('--mode', type=str, choices=['f', 'cf'], default='cf')
     parser.add_argument('--run_name', type=str, default='default')
     parser.add_argument('--proj_name', type=str, default='TransPath_runs')
     parser.add_argument('--seed', type=int, default=39)
@@ -59,7 +58,7 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
     pl.seed_everything(args.seed)
-    torch.set_float32_matmul_precision('high') #fix for tesor blocks warning with new video card
+    torch.set_float32_matmul_precision('high') #fix for tensor blocks warning with new video card
     main(
         mode=args.mode,
         run_name=args.run_name,
